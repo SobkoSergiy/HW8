195b9e10a73a502b27b524bed7fdefab24c78775
SobkoSergiy <143492394+SobkoSergiy@users.noreply.github.com>
2023-10-07 17:19:04 +0300

    Update BirthdayBell.py
    final

--- BirthdayBell.py
+++ BirthdayBell.py
@@ -1,29 +1,34 @@
-from datetime import datetime, timedelta
+from datetime import date, timedelta 
 
 week_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 def get_birthdays_per_week(users):
-    cur_date = datetime.today()   
+
+    cur_date = date.today()   
     delta = timedelta(days=1)
-  
+    wb = {}
+
     def select_users(day, month):
         ul = []
         for u in users:
             ud = u["birthday"]
             if ((ud.month == month) and (ud.day == day)):
-                ul.append(u)
+                ul.append(u["name"])
         return ul
-
-    def print_wl(wl, wd):
-        if len(wl) > 0:
-            print(week_list[wd]+": "+', '.join(p["name"] for p in wl))
 
     def create_wlist():
         wl = []
         for cd in range(7):
             d = cur_date + delta * cd
             wl.extend(select_users(d.day, d.month))
+
+            print(f"weekday:{d.weekday()} -> {week_list[d.weekday()]}")
+
             if d.weekday() < 5:
-                print_wl(wl, d.weekday())
+                if len(wl) > 0:
+                    wb[week_list[d.weekday()]] = wl[:]
                 wl.clear()
-    create_wlist()
+                
+    if len(users) > 0:
+        create_wlist()
+    return wb
